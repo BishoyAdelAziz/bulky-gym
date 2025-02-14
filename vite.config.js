362cc3a1db2fcdb086e7490531b0e2d850a53e76
Bishoy Adel <bishoy.adel5555@gmail.com>
2025-02-14 18:40:43 +0200

    configured netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    preset: "netlify",
+  },
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
     react(),
